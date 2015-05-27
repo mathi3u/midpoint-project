e4421fc0ce428c49d9adca8a6cac4ad8586f818d
Mathieu Romary <mro@Mathieus-MacBook-Pro.local>
2015-05-28 00:27:54 +0200

    Committer: mathi3u
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    	modified:   app.js
    	modified:   public/index.html
    
     Untracked files:
    	node_modules/

--- app.js
+++ app.js
@@ -6,48 +6,44 @@ var express = require('express'),
 
 var nicknames = {};
 
-
 app.use(express.static(path.join(__dirname, './public')));
 
 io.on('connection', function (socket) {
     console.log('INFO New connection. Socket id %s', socket.id);
 
-        socket.on('new user', function(n, callback){
-            console.log('INFO New user %s', n.nickname);
-            if (n.nickname in nicknames){
-                            console.log('WARN user %s already exists', n.nickname);
-                callback(false);
-            }
-            else{
-                callback(true);
-                socket.nickname = n.nickname;
-                                socket.location = n.location;
-                nicknames[socket.nickname] = { "nickname": socket.nickname, "location": socket.location };
-                console.log('INFO User %s has been added to the list', socket.nickname);
-                io.to(socket.id).emit('welcome', { "motd": "Welcome " + socket.nickname + "! An apple a day keeps the doctor away", "nicknames": nicknames });
-                io.emit('user joined', nicknames[socket.nickname]);
-                console.log(nicknames);
-            }
-        });
-
-        socket.on('send message', function (message){
-                console.log('INFO User %s message "%s"', socket.nickname, message);
-                io.emit('new message', { id: socket.id, nick: socket.nickname, msg: message, date: Date.now() });
-        });
-
-
-        socket.on('disconnect', function (){
-                        var user = nicknames[socket.nickname];
-
-            if (socket.nickname){
-                delete nicknames[socket.nickname];
-            }
-                console.log('User %s disconnected. Socket id %s', socket.nickname, socket.id);
-                        if (socket.nickname){
-                io.emit('user left', socket.nickname);
-                        }
+    socket.on('new user', function(n, callback){
+        console.log('INFO New user %s', n.nickname);
+        if (n.nickname in nicknames){
+            console.log('WARN user %s already exists', n.nickname);
+            callback(false);
+        }
+        else{
+            callback(true);
+            socket.nickname = n.nickname;
+            socket.location = n.location;
+            nicknames[socket.nickname] = { "nickname": socket.nickname, "location": socket.location };
+            console.log('INFO User %s has been added to the list', socket.nickname);
+            io.to(socket.id).emit('welcome', { "motd": "Welcome " + socket.nickname + "! An apple a day keeps the doctor away", "nicknames": nicknames });
+            io.emit('user joined', nicknames[socket.nickname]);
             console.log(nicknames);
-        });
+        }
+    });
+
+    socket.on('send message', function (message){
+       console.log('INFO User %s message "%s"', socket.nickname, message);
+       io.emit('new message', { id: socket.id, nick: socket.nickname, msg: message, date: Date.now() });
+    });
+
+    socket.on('disconnect', function (){
+        var user = nicknames[socket.nickname];
+
+        if (socket.nickname && nicknames[socket.nickname]){
+            io.emit('user left', nicknames[socket.nickname]);
+            delete nicknames[socket.nickname];
+        }
+        console.log('User %s disconnected. Socket id %s', socket.nickname, socket.id);
+        console.log(nicknames);
+    });
 });
 
 http.listen(3000, function () {
